2af5bf869920ad31b408b17241708e4b971df0a1
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-06 20:36:53 +0800

    Added L4Q6 answer

---
+++ Labs/Lab4/L4Q6.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class L4Q6 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        
+        // Generate a non-negative random number (0 inclusive)
+        int randomNo = random.nextInt(Integer.MAX_VALUE);
+        System.out.println("Positive Random No: " + randomNo);
+
+        // Handle the case where the random number is 0, which has 1 digit
+        int i = (randomNo == 0) ? 1 : 0;
+
+        // Count the number of digits
+        while (randomNo > 0) {
+            randomNo /= 10;
+            i++;
+        }
+
+        System.out.println("Number of digits: " + i);
+    }
+}
