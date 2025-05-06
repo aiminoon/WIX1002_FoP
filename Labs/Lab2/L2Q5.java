455a1cdd42f33a04f9b11896f16f1f205a759e33
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-06 20:19:07 +0800

    Added L2Q5 answer

---
+++ Labs/Lab2/L2Q5.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class L02Q05 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomVal = random.nextInt(10001);
+        System.out.println("Random No Generated: " + randomVal);
+        
+        int sum = 0;
+        for (int i = 10; i < 10001; i = i * 10) {
+            int temp = randomVal % i;
+            sum += temp / (i / 10);
+            randomVal -= temp;
+        }
+        System.out.println("Sum of the digits : " + sum);      
+    }
+}
