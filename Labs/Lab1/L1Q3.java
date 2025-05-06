7534997bc4b83add8d2ad22e3a7fb4892f2a0472
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-06 20:13:15 +0800

    Added L1Q3 answer

---
+++ Labs/Lab1/L1Q3.java
@@ -0,0 +1,13 @@
+public class L1Q3 {
+    public static void main(String[] args) {
+        System.out.println("*********          ***            *             *");
+        System.out.println("*       *        *     *         ***           * *");
+        System.out.println("*       *       *       *       *****         *   *");
+        System.out.println("*       *       *       *         *          *     *");
+        System.out.println("*       *       *       *         *         *       *");
+        System.out.println("*       *       *       *         *          *     *");
+        System.out.println("*       *       *       *         *           *   *");
+        System.out.println("*       *        *     *          *            * *");
+        System.out.println("*********          ***            *             *");
+    }
+}
