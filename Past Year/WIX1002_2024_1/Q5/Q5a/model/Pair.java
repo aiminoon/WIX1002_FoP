e5263c6160d966a695816e29dd486846e37a255f
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-06 18:09:05 +0800

    Create Pair.java

---
+++ Past Year/WIX1002_2024_1/Q5/Q5a/model/Pair.java
@@ -0,0 +1,11 @@
+package WIX1002_2024_1;
+
+public class Pair {
+    public final Player player1;
+    public final Player player2;
+
+    public Pair(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+}
