cc369b76d1bcc628f69addef2336022d6805da73
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-06 20:15:47 +0800

    Added L2Q1 answer

---
+++ Labs/Lab2/L2Q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class L2Q1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the temperature in degree Fahrenheit: ");
+        double fahrenheit = input.nextDouble();
+        System.out.println();
+        
+        double celsius = (fahrenheit - 32) / 1.8;
+        System.out.printf("Celsius = %.2f%n", celsius);
+        input.close();
+    }
+}
