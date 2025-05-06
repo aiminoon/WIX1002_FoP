e2d1958c39e6d5b6e12e02f7ed2cf80c1c6dac7b
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-06 21:03:23 +0800

    Added L7Q2 answer

---
+++ Labs/Lab7/L7Q2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.net.URL;
+import java.io.InputStream;
+import java.io.FileOutputStream;
+import java.net.URLConnection;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class L7Q2 {
+    public static void main(String[] args) {
+        try {
+            URL u = new URL("https://fsktm.um.edu.my/");
+            URLConnection cnn = u.openConnection();
+            InputStream stream = cnn.getInputStream();
+            Scanner in = new Scanner(stream);
+            PrintWriter out = new PrintWriter(new FileOutputStream("src/Lab7/index.htm"));
+            String line;
+            while(in.hasNextLine()) {
+                line = in.nextLine();
+                out.println(line);
+            }
+            
+            in.close();
+            out.close();
+            
+        } catch (IOException e) {
+            System.out.println("IO Error:" + e.getMessage());
+        }
+    }
+}
