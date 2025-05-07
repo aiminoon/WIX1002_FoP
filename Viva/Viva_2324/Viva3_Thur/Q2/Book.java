e1d144b6031bf0281636cd47a227500224d87f37
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-07 11:46:14 +0800

    Create Book.java

---
+++ Viva/Viva_2324/Viva3_Thur/Q2/Book.java
@@ -0,0 +1,37 @@
+package Viva3_Thur.Q2;
+
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getISBN() {
+        return isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setISBN(String isbn) {
+        this.isbn = isbn;
+    }
+}
