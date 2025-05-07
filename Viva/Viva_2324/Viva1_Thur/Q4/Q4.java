3024d98a98c878b3fb5c0f98d0fcfa365b7b8b5d
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-07 11:16:15 +0800

    Update and rename Main.java to Q4.java

--- Viva/Viva_2324/Viva1_Thur/Q4/Q4.java
+++ Viva/Viva_2324/Viva1_Thur/Q4/Q4.java
@@ -2,7 +2,7 @@ package Viva1_Thur.Q4;
 
 import java.util.Scanner;
 
-public class Main {
+public class Q4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
