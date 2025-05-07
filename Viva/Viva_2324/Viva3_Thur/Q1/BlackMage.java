e0d0f85eb291387e65786699f5ca09ce6bf03c84
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-07 11:41:22 +0800

    Create BlackMage.java

---
+++ Viva/Viva_2324/Viva3_Thur/Q1/BlackMage.java
@@ -0,0 +1,14 @@
+package Viva3_Thur.Q1;
+
+public class BlackMage {
+    int hp;
+    float evasion;
+    float critResistance;
+
+    // Default constructor
+    public BlackMage() {
+        this.hp = 100;
+        this.evasion = 0.05f;
+        this.critResistance = 0.1f;
+    }
+}
