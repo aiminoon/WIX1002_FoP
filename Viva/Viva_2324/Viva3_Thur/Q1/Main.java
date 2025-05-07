414f5873960d17109a6a7d4824adf80635013350
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-07 11:42:16 +0800

    Create Main.java

---
+++ Viva/Viva_2324/Viva3_Thur/Q1/Main.java
@@ -0,0 +1,8 @@
+package Viva3_Thur.Q1;
+
+public class Main {
+    public static void main(String[] args) {
+        Phantom phantom = new Phantom(0.8f, 0.2f);
+        System.out.println(phantom.toString());
+    }
+}
