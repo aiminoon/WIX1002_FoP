a5d9422978435e8625821adee0b5bd1302dea695
aiminoon <149665483+aiminoon@users.noreply.github.com>
2025-05-07 10:56:01 +0800

    Create Main.java

---
+++ Viva/Viva_2324/Viva1_Thur/Q1/Main.java
@@ -0,0 +1,17 @@
+package Viva_Thur.Q1;
+  
+public class Viva1_Thur.Q1 {   
+    public static void main(String[] args) {
+        int rowCount = 5;
+ 
+        for (int row = 1; row <= rowCount; row++) { 
+          for (int space = 1; space <= rowCount - row; space++)
+                System.out.print(" ");
+            for (int decNum = row; decNum >= 1; decNum--)
+              System.out.print(decNum);
+            for (int incNum = 2; incNum <= row; incNum++)
+                System.out.print(incNum);
+            System.out.println();
+        }
+    }
+}
